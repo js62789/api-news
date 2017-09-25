6ba63224781fead2c6980738362ca5e8709f1129
Jeffrey Smith <js62789@gmail.com>
2017-09-24 20:59:05 -0400

    Handle errors with catch handlers

--- src/routes/v1/router.js
+++ src/routes/v1/router.js
@@ -21,28 +21,30 @@ router.get('/', (req, res) => {
   });
 });
 
-router.get('/sources', function (req, res) {
+router.get('/sources', function (req, res, next) {
   return getSources()
     .then(sources => {
       res.send({
         sources
       });
-    });
+    })
+    .catch(next);
 });
 
-router.get('/sources/:source_id(\\d+)', function (req, res) {
+router.get('/sources/:source_id(\\d+)', function (req, res, next) {
   return getSourceById(req.params.source_id)
     .then(source => {
       if (!source) {
-        throw new NotFoundError('Feed Not Found');
+        throw new NotFoundError('Source Not Found');
       }
       res.send({
         sources: [ source ]
       });
-    });
+    })
+    .catch(next);
 });
 
-router.get('/sources/:source_id', function (req, res) {
+router.get('/sources/:source_id', function (req, res, next) {
   return getSourceByKey(req.params.source_id)
     .then(source => {
       if (!source) {
@@ -51,19 +53,21 @@ router.get('/sources/:source_id', function (req, res) {
       res.send({
         sources: [ source ]
       });
-    });
+    })
+    .catch(next);
 });
 
-router.get('/feeds', function (req, res) {
+router.get('/feeds', function (req, res, next) {
   return getFeeds()
     .then(feeds => {
       res.send({
         feeds
       });
-    });
+    })
+    .catch(next);
 });
 
-router.get('/feeds/:feed_id', function (req, res) {
+router.get('/feeds/:feed_id', function (req, res, next) {
   return getFeed(req.params.feed_id)
     .then(feed => {
       if (!feed) {
@@ -72,10 +76,11 @@ router.get('/feeds/:feed_id', function (req, res) {
       res.send({
         feeds: [ feed ]
       });
-    });
+    })
+    .catch(next);
 });
 
-router.get('/sources/:source(\\d+)/articles', function (req, res) {
+router.get('/sources/:source(\\d+)/articles', function (req, res, next) {
   const sourceId = req.params.source;
 
   return getSourceById(sourceId)
@@ -90,10 +95,11 @@ router.get('/sources/:source(\\d+)/articles', function (req, res) {
             articles: articles
           });
         });
-    });
+    })
+    .catch(next);
 });
 
-router.get('/sources/:source/articles', function (req, res) {
+router.get('/sources/:source/articles', function (req, res, next) {
   const sourceKey = req.params.source;
 
   return getSourceByKey(sourceKey)
@@ -108,10 +114,11 @@ router.get('/sources/:source/articles', function (req, res) {
             articles: articles
           });
         });
-    });
+    })
+    .catch(next);
 });
 
-router.get('/articles', function (req, res) {
+router.get('/articles', function (req, res, next) {
   const feed = req.query.feed;
 
   if (!feed) {
@@ -123,16 +130,18 @@ router.get('/articles', function (req, res) {
       res.send({
         articles
       });
-    });
+    })
+    .catch(next);
 });
 
-router.get('/articles/:guid', function (req, res) {
+router.get('/articles/:guid', function (req, res, next) {
   return getArticleByGUID(req.params.guid)
     .then((article) => {
       res.send({
         articles: [ article ]
       });
-    });
+    })
+    .catch(next);
 });
 
 module.exports = router;
